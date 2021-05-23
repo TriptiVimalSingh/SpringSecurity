ff729ce546179b0f3170e95e364fe5a4e3c52b8f
Tripti Vimal <vimal.tripti.singh@gmail.com>
2021-05-23 22:38:04 +0530

    author name added

--- DemoSecurity/src/main/java/com/example/DemoSecurity/DemoSecurityApplication.java
+++ DemoSecurity/src/main/java/com/example/DemoSecurity/DemoSecurityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoSecurityApplication {
-
+// tripti
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSecurityApplication.class, args);
 	}
